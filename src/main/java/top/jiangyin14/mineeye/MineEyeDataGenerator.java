8f51bcf5b681d8a4da9384a24be2b517826e29c6
姜胤 <1219439938@qq.com>
2025-03-16 08:14:06 +0800

    Init

---
+++ src/main/java/top/jiangyin14/mineeye/MineEyeDataGenerator.java
@@ -0,0 +1,11 @@
+package top.jiangyin14.mineeye;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MineEyeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
